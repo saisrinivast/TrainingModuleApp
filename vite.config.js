1b963dbe98ce0b8ffc242e25462dd8496dd7fdbd
saisrinivast <srinivassai617@gmail.com>
2025-06-18 20:29:03 +0530

    tenth

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    port: 3000
-  }
-});
+  base: './',  
+})
