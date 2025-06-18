2cfdddd253d9f5f39e23eecdc44c57767528a3ef
saisrinivast <srinivassai617@gmail.com>
2025-06-18 20:39:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // 👈 important for Vercel
   plugins: [react()],
-  base: './',  
-})
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000
+  }
+});
